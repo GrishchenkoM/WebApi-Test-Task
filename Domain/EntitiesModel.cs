ec2bfa0cfeb0af86126b5abcf856071115feafa4
Michael <grishchenko.m@yandex.ru>
2016-04-27 20:38:44 +0300

    Create entities and entitiesModel for info returning

---
+++ Domain/EntitiesModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain
+{
+    public class EntitiesModel
+    {
+        public ComputerName ComputerName { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public IList<UserName> UserNames { get; set; }
+    }
+}
