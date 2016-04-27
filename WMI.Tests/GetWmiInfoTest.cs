155ce87b51c2fcf8ed3841ac9b85abde343649ff
Michael <grishchenko.m@yandex.ru>
2016-04-28 00:01:21 +0300

    create WMI.Test project

---
+++ WMI.Tests/GetWmiInfoTest.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using NUnit.Framework;
+
+namespace WMI.Tests
+{
+    [TestFixture]
+    public class GetWmiInfoTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _model = new WmInfo().GetInfo();
+        }
+
+        [Test]
+        public void GetInfo_Manufacturer_IsNotEmpty_Test()
+        {
+            Assert.IsNotEmpty(_model.Manufacturer.Name);
+        }
+
+        [Test]
+        public void GetInfo_ComputerName_IsNotEmpty_Test()
+        {
+            Assert.IsNotEmpty(_model.ComputerName.Name);
+        }
+
+        [Test]
+        public void GetInfo_UserNames_IsNotEmpty_Test()
+        {
+            Assert.IsNotEmpty(_model.UserNames);
+        }
+
+        EntitiesModel _model;
+    }
+}
