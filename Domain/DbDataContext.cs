01f756686ebf0163982e2e446e02371ef89e0c00
Michael <grishchenko.m@yandex.ru>
2016-04-28 13:18:59 +0300

    create DbDataContext in Domain proj and add entity framework ref

---
+++ Domain/DbDataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using Domain.Entities;
+
+namespace Domain
+{
+    public class DbDataContext : DbContext
+    {
+        public DbDataContext()
+            : base("DbDataContext")
+        { }
+        public DbSet<ComputerName> ComputerNames { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<UserName> UserNames { get; set; }
+    }
+}
