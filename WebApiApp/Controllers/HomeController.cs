afddc23a791ba328ae8432c29b5090075e190a20
Michael <grishchenko.m@yandex.ru>
2016-04-28 21:14:37 +0300

    Create WebApiApp project

---
+++ WebApiApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using DbLogic;
+using Management;
+using WMI;
+
+namespace WebApiApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(DataManager dataManager)
+        {
+            _dataManager = dataManager;
+
+            var _manager = new Manager(_dataManager, new WmInfo());
+            _manager.Manage();
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        private readonly DataManager _dataManager;
+        private readonly Manager _manager;
+    }
+}
---
+++ WebApiApp/NinjectControllerFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DbLogic.Repositories.Implementations;
+using DbLogic.Repositories.Interfaces;
+using Domain;
+using Ninject;
+
+namespace WebApiApp
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        public NinjectControllerFactory()
+        {
+            _ninjectKernel = new StandardKernel();
+            AddBinding();
+        }
+
+        // извлекаем экземпляр контроллера для заданного контекста запроса и типа контроллера   
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)_ninjectKernel.Get(controllerType);
+        }
+
+        // опрделим все привязки   
+        private void AddBinding()
+        {
+            _ninjectKernel.Bind<IComputerRepository>().To<ComputerRepository>();
+            _ninjectKernel.Bind<IManufacturerRepository>().To<ManufacturerRepository>();
+            _ninjectKernel.Bind<IUserRepository>().To<UserRepository>();
+            
+            _ninjectKernel.Bind<DbDataContext>()
+                          .ToSelf()
+                          .WithConstructorArgument("connectionString",
+                                                   ConfigurationManager.ConnectionStrings[0].ConnectionString);
+            
+        }
+
+        private readonly IKernel _ninjectKernel;
+    }
+}
