6b9c4c590b32b7c58563f0fbcf430bd75d6c140d
Michael <grishchenko.m@yandex.ru>
2016-04-28 21:03:14 +0300

    Create Timer class

---
+++ Management/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+
+namespace Management
+{
+    public class Timer
+    {
+        public Timer(Manager manager)
+        {
+            _manager = manager;
+        }
+        public Timer(Manager manager, int updateTime) : this(manager)
+        {
+            _updateTime = updateTime;
+        }
+        public void SwitchOn()
+        {
+            var adapter = new Adapter(_manager);
+            var tm = new TimerCallback(adapter.Manage);
+            var timer = new System.Threading.Timer(tm, null, 0, _updateTime * 1000);
+        }
+
+        private readonly Manager _manager;
+        private readonly int _updateTime = 3600; // 60 minutes
+    }
+
+    internal class Adapter
+    {
+        public Adapter(Manager manager)
+        {
+            _manager = manager;
+        }
+
+        public void Manage(object o)
+        {
+            _manager.Manage();
+        }
+
+        private readonly Manager _manager;
+    }
+}
