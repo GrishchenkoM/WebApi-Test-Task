d32bd3335d800c27532d55715388d84e14817ae1
Michael <grishchenko.m@yandex.ru>
2016-04-29 14:46:07 +0300

    Create ViewModel

---
+++ WebApiApp/Models/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using DbLogic;
+using Domain;
+using Management;
+
+namespace WebApiApp.Models
+{
+    public class ViewModel
+    {
+        public ViewModel(DataManager manager)
+        {
+            _manager = manager;
+            FillModel();
+        }
+
+        public ViewModel(DataManager dataManager, Manager manager)
+        {
+            _manager = dataManager;
+            manager.Manage();
+            FillModel();
+        }
+
+        private void FillModel()
+        {
+            try
+            {
+                Model = new EntitiesModel()
+                {
+                    Manufacturer = _manager.ManufacturerRepositories.GetManufacturers().FirstOrDefault(),
+                    Computer = _manager.ComputerRepositories.GetComputers().FirstOrDefault(),
+                    Users = _manager.UserRepositories.GetUsers().ToList()
+                };
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        public EntitiesModel Model { get; private set; }
+        private readonly DataManager _manager;
+    }
+}
