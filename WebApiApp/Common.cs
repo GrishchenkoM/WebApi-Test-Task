80b4799950ed70369f7f0d79b438b1f1cef623d5
Michael <grishchenko.m@yandex.ru>
2016-04-29 14:45:25 +0300

    Create Common class for DataManager and Manager objects provision to ValuesController

---
+++ WebApiApp/Common.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DbLogic;
+using Management;
+using WMI;
+
+namespace WebApiApp
+{
+    public static class Common
+    {
+        public static DataManager DataManager { get; set; }
+
+        public static Manager Manager { get; set; }
+    }
+}
